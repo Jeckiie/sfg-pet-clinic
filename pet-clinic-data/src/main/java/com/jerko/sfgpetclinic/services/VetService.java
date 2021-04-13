4bdab28d614b20f6d7ecc95b3da82c746caadf30
jerkokosic <jerkokosic1@gmail.com>
2021-04-13 12:24:07 +0200

    Implement Interfaces for services. Closes #4.

---
+++ pet-clinic-data/src/main/java/com/jerko/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.jerko.sfgpetclinic.services;
+
+import com.jerko.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
