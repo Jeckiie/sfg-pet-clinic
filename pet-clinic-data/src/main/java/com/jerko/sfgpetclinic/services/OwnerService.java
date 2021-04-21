c5d4e400c9140d88c6d40d8b6f55170acf831141
jerkokosic <jerkokosic1@gmail.com>
2021-04-21 12:10:50 +0200

    added templates and find owner functionality

--- pet-clinic-data/src/main/java/com/jerko/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/jerko/sfgpetclinic/services/OwnerService.java
@@ -2,8 +2,10 @@ package com.jerko.sfgpetclinic.services;
 
 import com.jerko.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-
+    List<Owner> findAllByLastNameLike(String lastName);
 }
---
+++ pet-clinic-web/src/test/java/com/jerko/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,37 @@
+package com.jerko.sfgpetclinic.controllers;
+
+import com.jerko.sfgpetclinic.model.Owner;
+import com.jerko.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+    @InjectMocks
+    OwnerController controller;
+    Set<Owner> owners;
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1L).build());
+        owners.add(Owner.builder().id(2L).build());
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
+    }
+
+    @Test
+    void findOwners() {
+    }
+}
